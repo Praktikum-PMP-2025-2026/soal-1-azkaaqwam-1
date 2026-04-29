1336cb7109a1e0a596b224d034b23b60d81ede42
azkaaqwam <azkamhd08@gmail.com>
2026-04-29 10:16:51 +0700

    Update soal1.c

--- soal1.c
+++ soal1.c
@@ -21,7 +21,6 @@ int bandingkan_artefak(const void *a, const void *b) {
     Artefak *m2 = (Artefak *)b;
     int c = strcmp(m1->kategori,m2->kategori);
     if (c!=0) return c;
-    return strcmp(m1->kategori,m2->kategori);
     if (m1->tahun != m2->tahun) {
         return m2->tahun - m1->tahun;
     }
